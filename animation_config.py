68f3f4eef6be478b817da0416eb29268885e3897
Taylor Thomas <taylort@barolo.eol.ucar.edu>
2021-08-02 16:40:55 -0600

    fix to min and max selection with np.nan()

--- animation_config.py
+++ animation_config.py
@@ -5,29 +5,29 @@
 #######################################################################
 
 # Provide project, flight, and location of data file
-project = "<PROJECT>"
-flight = "<XXXX>"
-dat = ""
-flight_data = ""
+project = "SPICULE"
+flight = "rf09"
+dat = "/scr/raf_data/SPICULE/field_data/"
+flight_data = "/scr/raf_data/SPICULE/field_data/SPICULErf09.nc"
 
 # Define where the existing digital camera movies are located
-flight_movie_dir = ""
+flight_movie_dir = "/scr/raf_Raw_Data/SPICULE/Movies/"
 
 # Define where you would like the output .mp4 to be written
-output_dir = ""
+output_dir = "/scr/raf_Raw_Data/SPICULE/Movies/animations/"
 
 # Animation variable selection
-Var1 = ""
-Var2 = ""
-Var3 = ""
-Var4 = ""
-Var5 = ""
-Var6 = ""
-Var7 = ""
-Var8 = ""
+Var1 = "GGALT"
+Var2 = "CONC1DC_RWOO"
+Var3 = "PSX"
+Var4 = "WIC"
+Var5 = "ATX"
+Var6 = "CONCP_LWOI"
+Var7 = "ATX"
+Var8 = "GGALT"
 
 # Plot formatting options
-dpi = 200 # Lower res will animate faster
+dpi = 400 # Lower res will animate faster
 fps = 15 # Script checks length against movie file, but this can be set faster or slower at onset
 LineColor = "blue"
-width="712:"
+width="450:"
