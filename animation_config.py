94f2905dfdd1b091272b695a1b421fcc634c2b1b
Janine Aquino <janine@ucar.edu>
2026-06-26 21:32:10 +0000

    Project this config relates to is defined by it's location, but go ahead and sub in the project name when it's seeded

--- animation_config.py
+++ animation_config.py
@@ -6,32 +6,26 @@
 
 import os
 
-# Provide project, flight
-project = "CAESAR"
-flights = ["rf09","rf10"] ##must be a list even if only processing one flight
+### Configuration is for project <project> ###
 
-# Uncomment and edit one of the following types of plots
-# Example animation variable selection with two vars in one plot
-#Var1 = "GGALT"
-#Var2 = "PSX"
-#Var3 = "WIC"
-#Var4 = "PLWCC"
-#Var5 = "CONCS_2DS"
-#Var6 = "CONCD_LWI"
-#Var7 = "FO3C_ACD"
-#Var7a = "ATX"
-#Var7b = "DPXC"
-#VARLIST = [Var1,Var2,Var7a,Var7b,Var3,Var4,Var5,Var6,Var7,(Var1,Var7a, Var7b),('GGLON', 'GGLAT')]
+# Provide flight: flights must be a list even if only processing one flight
+flights = ["rfxx"]
 
-# Example animation variable selection with two vars plotted against each other
-#Var1 = "GGALT"
-#Var2 = "DPXC"
-#Var3 = "PSX"
-#Var4 = "WIC"
-#Var5 = "ATX"
-#Var6 = "CONCD_RWO"
-#Var7 = "ATX"
-#VARLIST = [Var1,Var2,Var3,Var4,Var5,Var6,(Var1,Var7), ('GGLON', 'GGLAT')]
+# Plots are generated one plot per value in varlist
+# Variables within parenthesis are plotted on a single plots
+# - Var1 without parens will plot Var1 vs time
+# - (Var1, Var2) will plot Var1 on X-axis, Var2 on left Y-axis
+# - (Var1, Var2, Var3) will plot Var1 on X-axis, Var2 on left Y-axis and
+#    Var3 on right Y-axis
+# 
+# Add/edit as requested for the project
+Var1 = "GGALT"
+Var2 = "DPXC"
+Var3 = "PSX"
+Var4 = "WIC"
+Var5 = "ATX"
+Var6 = "CONCD_xxx"
+VARLIST = [Var1,Var2,Var3,Var4,Var5,Var6,(Var1,Var5), ('GGLON', 'GGLAT')]
 
 # Plot formatting options
 dpi = 400 # Lower res will animate faster
